190c2ab5d2a832e7120fddc4fb0806aca915cea9
Puru Vijay <awesomepuruvj@gmail.com>
2021-12-19 16:33:04 +0530

    Ensure My Day Tasks

--- src/routes/[listID].svelte
+++ src/routes/[listID].svelte
@@ -1,11 +1,50 @@
 <script>
+  import { pullFromArray } from '$/helpers/array';
+
   import { page } from '$app/stores';
   import ListView from '$components/List/ListView.svelte';
   import SideNav from '$components/SideNav.svelte';
   import NewTask from '$components/Task/NewTask.svelte';
   import { lists } from '$stores/lists.store';
+  import { tasks } from '$stores/tasks.store';
+  import { endOfDay, isAfter, isPast } from 'date-fns';
+  import { onMount } from 'svelte';
 
   $: list = $lists[$page.params.listID];
+
+  onMount(() => {
+    ensureMyDayTasks();
+  });
+
+  /**
+   * Make sure old tasks are removed from `my-day` list
+   */
+  function ensureMyDayTasks() {
+    for (let [taskID, { dateDue, listIDs }] of Object.entries($tasks)) {
+      dateDue = new Date(dateDue);
+
+      // If task is not in `my-day` list and is before due date
+      if (!isPast(dateDue) && !listIDs.includes('my-day')) {
+        $tasks[taskID].listIDs.push('my-day');
+      }
+
+      // If task is in `my-day` list and is after due date
+      if (isPast(dateDue) && listIDs.includes('my-day')) {
+        // Remove `my-day`
+        $tasks[taskID].listIDs = pullFromArray(listIDs, 'my-day');
+      }
+
+      // If due date is after the end of today and task is not in `planned` list
+      if (isAfter(dateDue, endOfDay(new Date())) && !listIDs.includes('planned')) {
+        $tasks[taskID].listIDs.push('planned');
+      }
+
+      // If due date is before the end of today and task is in `planned` list
+      if (!isAfter(dateDue, endOfDay(new Date())) && listIDs.includes('planned')) {
+        $tasks[taskID].listIDs = pullFromArray(listIDs, 'planned');
+      }
+    }
+  }
 </script>
 
 <div class="container">
