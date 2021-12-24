0f4f7103d46db670be3b176c4a991d9d73b697c9
Puru Vijay <awesomepuruvj@gmail.com>
2021-12-24 13:45:30 +0530

    Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import staticAdapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: staticAdapter(),
+    adapter: adapter(),
 
     prerender: {
       crawl: true,
